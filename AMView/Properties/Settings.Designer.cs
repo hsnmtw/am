cb6e5f5755fd88a94e4a07a16b275fe2b56d8610
hsnmtw <hussain.mutawa@gmail.com>
2020-07-20 07:46:11 +0300

    fixed directories naming

--- AMView/Properties/Settings.Designer.cs
+++ AMView/Properties/Settings.Designer.cs
@@ -22,15 +22,5 @@ namespace AMView.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\\AssetManager.mdb")]
-        public string AssetManagerConnectionString {
-            get {
-                return ((string)(this["AssetManagerConnectionString"]));
-            }
-        }
     }
 }
